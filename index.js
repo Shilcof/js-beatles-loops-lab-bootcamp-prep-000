155aadcef8a1f34aec8eba138b445c34f662a7f4
Robert Shilcof <robert.shilcof@gmail.com>
2020-09-08 12:47:46 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ var array = [];
 
 
 function theBeatlesPlay(musicians, instruments) {
-  for (let i = 0; i < musicians.length(); i++ ) {
+  for (let i = 0; i < musicians.length; i++ ) {
     array.push(`"${musicians[i]} plays ${instruments[i]}"`)
   }
   return array
